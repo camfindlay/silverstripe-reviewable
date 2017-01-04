ea5697d2e182960f77fbb794ae56d33e551cafe6
Michael James <MichaelJJames@users.noreply.github.com>
2017-01-04 15:20:37 +0000

    Average Ratings
    Added a function to work out average rating and return a star rating

--- code/ControllerExtension.php
+++ code/ControllerExtension.php
@@ -36,6 +36,39 @@ class ControllerExtension extends \DataExtension {
         $this->owner->redirectBack();
 
     }
+    
+    public function averageRating() {
+    
+        if($rating = \Reviewable\Review::get()->filter('ObjectID', $this->owner->ID)){
+
+            $countRating = $rating->Count();
+            $ratingTotal = 0;
+
+            foreach($rating as $review) {
+
+                $ratingTotal += $review->Rating;
+            
+            }
+
+            if(round($ratingTotal / $countRating) == 0) {
+                return '<img src="/reviewable/images/star-off.png" alt="1" title="bad"> <img src="/reviewable/images/star-off.png" alt="2" title="poor"> <img src="/reviewable/images/star-off.png" alt="3" title="regular"> <img src="/reviewable/images/star-off.png" alt="4" title="good"> <img src="/reviewable/images/star-off.png" alt="5" title="gorgeous">';
+            }elseif(round($ratingTotal / $countRating) == 1){
+                return '<img src="/reviewable/images/star-on.png" alt="1" title="bad"> <img src="/reviewable/images/star-off.png" alt="2" title="poor"> <img src="/reviewable/images/star-off.png" alt="3" title="regular"> <img src="/reviewable/images/star-off.png" alt="4" title="good"> <img src="/reviewable/images/star-off.png" alt="5" title="gorgeous">';
+            }elseif(round($ratingTotal / $countRating) == 2){
+                return '<img src="/reviewable/images/star-on.png" alt="1" title="bad"> <img src="/reviewable/images/star-on.png" alt="2" title="poor"> <img src="/reviewable/images/star-off.png" alt="3" title="regular"> <img src="/reviewable/images/star-off.png" alt="4" title="good"> <img src="/reviewable/images/star-off.png" alt="5" title="gorgeous">';
+            }elseif(round($ratingTotal / $countRating) == 3){
+                return '<img src="/reviewable/images/star-on.png" alt="1" title="bad"> <img src="/reviewable/images/star-on.png" alt="2" title="poor"> <img src="/reviewable/images/star-on.png" alt="3" title="regular"> <img src="/reviewable/images/star-off.png" alt="4" title="good"> <img src="/reviewable/images/star-off.png" alt="5" title="gorgeous">';
+            }elseif(round($ratingTotal / $countRating) == 4){
+                return '<img src="/reviewable/images/star-on.png" alt="1" title="bad"> <img src="/reviewable/images/star-on.png" alt="2" title="poor"> <img src="/reviewable/images/star-on.png" alt="3" title="regular"> <img src="/reviewable/images/star-on.png" alt="4" title="good"> <img src="/reviewable/images/star-off.png" alt="5" title="gorgeous">';
+            }elseif(round($ratingTotal / $countRating) == 5){
+                return '<img src="/reviewable/images/star-on.png" alt="1" title="bad"> <img src="/reviewable/images/star-on.png" alt="2" title="poor"> <img src="/reviewable/images/star-on.png" alt="3" title="regular"> <img src="/reviewable/images/star-on.png" alt="4" title="good"> <img src="/reviewable/images/star-on.png" alt="5" title="gorgeous">';
+            }
+            
+        }
+
+        return '<img src="/reviewable/images/star-off.png" alt="1" title="bad"> <img src="/reviewable/images/star-off.png" alt="2" title="poor"> <img src="/reviewable/images/star-off.png" alt="3" title="regular"> <img src="/reviewable/images/star-off.png" alt="4" title="good"> <img src="/reviewable/images/star-off.png" alt="5" title="gorgeous">';
+
+    }
 
         
 }
